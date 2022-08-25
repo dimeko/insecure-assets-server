042d0be90bf8d5549ae9c9eb9110f446d1896b28
dimoiko100 <dimoiko100@gmail.com>
2022-08-25 03:24:15 +0300

    Adds support for image listing

--- get_image_dir_map.php
+++ get_image_dir_map.php
@@ -6,7 +6,17 @@ require "utils/env.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
     header('Content-type: application/json');
-    $dir = mapDirectories($APP_IMG_PATH);
+    $url = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+   
+    $components = parse_url($url);
+    parse_str($components['query'], $results);
+    $subDir = $results['subDir'] ?? null;
+    if($subDir) {
+        $dir = mapDirectoriesWithFiles($APP_IMG_PATH."/".$subDir);
+    } else {
+        $dir = mapDirectories($APP_IMG_PATH);
+    }
+    
     if($dir) {
         try {
             http_responder('SUCCESS', $dir);
