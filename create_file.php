19264840073348297941b49aa5b67766a58a5419
dimoiko100 <dimoiko100@gmail.com>
2023-01-15 17:45:43 +0200

    Fixes create_file

--- create_file.php
+++ create_file.php
@@ -10,11 +10,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $fileName = $data['file_name'];
     $subfolder = $data['subfolder'] === "" ? "/" : $data['subfolder'];
     $newContent = $data['data'];
-
+    $new_file_path = $APP_BASE_PATH . $subfolder . $fileName;
+    $new_folder_path = $APP_BASE_PATH . $subfolder;
     header('Content-type: application/json');
     if ($fileName) {
-        file_put_contents($APP_BASE_PATH . $subfolder . $fileName, json_encode($newContent));
-        http_responder('SUCCESS',['data' => $newContent,'directory' => $APP_BASE_PATH . $subfolder]);
+        mkdir($new_folder_path, 0777, true);
+        $new_file = file_put_contents($new_file_path, json_encode($newContent));
+
+        http_responder('SUCCESS',['data' => $newContent,'directory' => $new_file_path]);
        
     } else {
         http_responder('FAIL','File not found');
